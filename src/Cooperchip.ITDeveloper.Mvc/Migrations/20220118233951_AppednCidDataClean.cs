92abaf2f962f08948001d2d8d0ef2b787f10064e
jairrss <jairrss@hotmail.com>
2022-01-18 20:45:57 -0300

    Atualização das migrations

---
+++ src/Cooperchip.ITDeveloper.Mvc/Migrations/20220118233951_AppednCidDataClean.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cooperchip.ITDeveloper.Mvc.Migrations
+{
+    public partial class AppednCidDataClean : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { 
+
+        }
+
+    }
+}
