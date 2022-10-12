481079ad506d26c09df32833e1904cb7471362f3
jairrss <jairrss@hotmail.com>
2022-10-12 15:50:46 -0300

    Validação Paciente via FluentValidation

---
+++ src/Cooperchip.ITDeveloper.Domain/Validations/Helpers/StringUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace Cooperchip.ITDeveloper.Domain.Validations.Helpers
+{
+    public class StringUtils
+    {
+        public static string ApenasNumeros(string valor)
+        {
+            var onyNumber = "";
+            foreach (var s in valor)
+            {
+                if (char.IsDigit(s))
+                {
+                    onyNumber += s;
+                }
+            }
+            return onyNumber;
+        }
+    }
+}
