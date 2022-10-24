ef3e7028db7700deb7c85dfe80620eb4f03ff306
jairrss <jairrss@hotmail.com>
2022-10-24 20:29:47 -0300

    Finalizando a Aula 273 implementação do microserviço Mural

--- src/Cooperchip.ITDeveloper.Mvc/Configuration/MvcAndRazor.cs
+++ src/Cooperchip.ITDeveloper.Mvc/Configuration/MvcAndRazor.cs
@@ -1,8 +1,9 @@
-﻿using Cooperchip.ITDeveloper.Mvc.Extensions.Filters;
+﻿
+using Cooperchip.ITDeveloper.Mvc.Extensions.Filters;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Cooperchip.ITDeveloper.Mvc.Configurations
+namespace Cooperchip.ITDeveloper.Mvc.Configuration
 {
     public static class MvcAndRazorConfig
     {
@@ -11,15 +12,32 @@ namespace Cooperchip.ITDeveloper.Mvc.Configurations
 
             services.AddMvc(options =>
             {
-                // Todo: Resolver DI;
                 options.Filters.Add(typeof(AuditoriaIloggerFilter));
+            });
 
-            }).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            services.AddControllersWithViews(o =>
+            {
+                o.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O valor preenchido é inválido para este campo.");
+                o.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(x => "Este campo precisa ser preenchido.");
+                o.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido.");
+                o.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que o body na requisição não esteja vazio.");
+                o.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
+                o.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(() => "O valor preenchido é inválido para este campo.");
+                o.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numérico");
+                o.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
+                o.ModelBindingMessageProvider.SetValueIsInvalidAccessor(x => "O valor preenchido é inválido para este campo.");
+                o.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(x => "O campo deve ser numérico.");
+                o.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(x => "Este campo precisa ser preenchido.");
+
+                o.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+            }).AddRazorRuntimeCompilation();
+
+
             services.AddRazorPages();
-
             return services;
+
         }
     }
 }
